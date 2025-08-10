c1b40db92f6fa1649a856d7cc9ef5ec44e044023
DylanCode <dylanmoukoko56@gmail.com>
2025-08-10 05:23:18 +0100

    Correction du chemin d'import dans App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from './pages/login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import DashboardLayout from './components/DashboardLayout';
-import Users from './pages/users';
+import Users from './pages/Users';
 import Stagiaires from './pages/Stagiaires';
 import Cellules from './pages/Cellules';
 import Encadreurs from './pages/Encadreurs';
@@ -13,12 +13,12 @@ import UserProfil from './pages/UserProfil';
 import LoginGoogleSuccess from './pages/LoginGoogleSuccess';
 
 function App() {
- 
+
 
   return (
-    
+
       <BrowserRouter>
-      
+
       <Routes>
         <Route path='/' element={<DashboardLayout/>}>
           <Route index element={<Dashboard/>}/>
@@ -33,7 +33,7 @@ function App() {
         <Route path='/forgotPassword' element={<ForgotPassword/>}></Route>
         <Route path='/login-success' element={<LoginGoogleSuccess/>}></Route>
       </Routes>
-      
+
       </BrowserRouter>
   );
 }
